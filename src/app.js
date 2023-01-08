610e934f23ed6d4381b291082f15491df923d30f
FredBettecher <fredbettecher@gmail.com>
2023-01-08 17:04:48 -0300

    post user working

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+app.use(express.json());
 app.use(cors());
+const PORT = 5000;
 
-app.get('/', ((req, res) => {
-    res.send("Olá")
+const users = [];
+const tweets = [];
+
+app.post('/sign-up', ((req, res) => {
+    const userData = req.body;
+
+    if(userData.username === "" || userData.avatar === "") {
+        res.status(422).send("O usuário deve preencher todos os campos");
+        return;
+    }
+
+    users.push(userData);
+    res.status(201).send("OK");
 }));
 
-app.listen(5000);
+app.listen(PORT);
