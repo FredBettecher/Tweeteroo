635c096c7f89944c3484225bcf25f882b342e649
FredBettecher <fredbettecher@gmail.com>
2023-01-08 18:06:00 -0300

    sign-up alteration, tweets implemented

--- src/app.js
+++ src/app.js
@@ -4,20 +4,44 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 const PORT = 5000;
 
 const users = [];
 const tweets = [];
+let logged = false;
 
 app.post('/sign-up', ((req, res) => {
     const userData = req.body;
 
     if(userData.username === "" || userData.avatar === "") {
-        res.status(422).send("O usuário deve preencher todos os campos");
+        res.status(422).send();
+        alert("O usuário deve preencher todos os campos");
         return;
     }
 
     users.push(userData);
+    console.log(users);
+    logged = true;
+    res.status(201).send("OK");
+}));
+
+app.post('/tweets', ((req, res) => {
+    const tweetsData = req.body;
+
+    if(!logged) {
+        res.status(401).send("UNAUTHORIZED");
+        return;
+    }
+
+    if(tweetsData.username === '' || tweetsData.tweet === '') {
+        res.status(422).send();
+        alert("O usuário deve preencher todos os campos");
+        return;
+    }
+    
+    tweets.push(tweetsData);
+    console.log(tweets);
     res.status(201).send("OK");
 }));
 
